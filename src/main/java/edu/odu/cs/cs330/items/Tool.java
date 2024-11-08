f5a1fc74c0a65f8fabd7bd5af1685726aafcb2c0
abuabdiAllah <daifadam04@gmail.com>
2024-11-08 02:33:39 +0100

    Add files via upload

---
+++ src/main/java/edu/odu/cs/cs330/items/Tool.java
@@ -0,0 +1,162 @@
+package edu.odu.cs.cs330.items;
+
+/**
+ * This class represents one tool--as found in most video games. This includes
+ * pickaxes and shovels.
+ *
+ * Tools may not be stacked. All Constructors must initialize Item::stackable
+ * to false.
+ */
+@SuppressWarnings({
+    "PMD.CloneMethodReturnTypeMustMatchClassName",
+    "PMD.CloneThrowsCloneNotSupportedException",
+    "PMD.LawOfDemeter",
+    "PMD.OnlyOneReturn",
+    "PMD.ProperCloneImplementation",
+    "PMD.MethodArgumentCouldBeFinal",
+    "PMD.LocalVariableCouldBeFinal",
+    "PMD.ShortClassName",
+})
+public class Tool extends Equippable {
+    /**
+     * Format used to generate a printable representation.
+     */
+    public static final String FMT_STR = String.join(
+        "",
+        "  Nme: %s%n",
+        "  Dur: %d%n",
+        "  Spd: %d%n",
+        "  Mtl: %s%n",
+        "  Mdr: %s (Lvl %d)%n"
+    );
+
+    /**
+     * Base operation (e.g., harvest/mine) speed.
+     */
+    protected int speed;
+
+    /**
+     * Default to an unstackable tool with zero speed.
+     */
+    public Tool()
+    {
+        super();
+
+        this.speed = 0;
+    }
+
+    /**
+     * Retrieve tool speed.
+     *
+     * @return how quickly a tool operates
+     */
+    public int getSpeed()
+    {
+        return this.speed;
+    }
+
+    /**
+     * Set tool speed.
+     *
+     * @param spd new speed
+     */
+    public void setSpeed(int spd)
+    {
+        this.speed = spd;
+    }
+
+    @Override
+    public int requiredNumberOfValues()
+    {
+        // Replace this with the correct value
+        return 6;
+    }
+
+    @Override
+    public void fromTokens(String[] tokens)
+    {
+        this.setName(tokens[0]);
+        this.setMaterial(tokens[1]);
+        this.setDurability(Integer.parseInt(tokens[2]));
+        // Complete the remainder of this method
+        this.setSpeed(Integer.parseInt(tokens[3]));
+        this.setModifier(tokens[4]);
+        this.setModifierLevel(Integer.parseInt(tokens[5]));
+    }
+
+    /**
+     * Clone--i.e., copy--this Tool.
+     */
+    @Override
+    public Item clone()
+    {
+        Tool cpy = new Tool();
+        // Complete the remainder of this method
+        cpy.setName(this.getName());
+        cpy.setDurability(this.getDurability());
+        cpy.setSpeed(getSpeed());
+        cpy.setMaterial(this.getMaterial());
+        cpy.setModifier(this.getModifier());
+        cpy.setModifierLevel(this.getModifierLevel());
+        return cpy;
+    }
+
+    /**
+     * Check for logical equivalence--based on name, speed, material, modifier,
+     * and modifierLevel
+     *
+     * @param rhs object for which a comparison is desired
+     */
+    @Override
+    public boolean equals(Object rhs)
+    {
+        if (!(rhs instanceof Tool)) {
+            return false;
+        }
+
+        Tool rhsItem = (Tool) rhs;
+        if((this.getName()==rhsItem.getName()) && (this.getMaterial()==rhsItem.getMaterial()) && 
+        (this.getModifier()==rhsItem.getModifier()) && (this.getModifierLevel()==rhsItem.getModifierLevel()) 
+        && (this.getSpeed()==rhsItem.getSpeed()))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * Compute hashCode using name, speed, material, modifier,
+     * and modifierLevel.
+     */
+    @Override
+    public int hashCode()
+    {
+        int hash = this.getName().hashCode();
+        hash += 2 * this.getMaterial().hashCode();
+        hash += 4 * this.getModifier().hashCode();
+        hash += 8 * this.getModifierLevel();
+        hash += 32 * this.getSpeed();
+
+        return hash;
+    }
+
+    /**
+     * *Print* a Tool.
+     */
+    @Override
+    public String toString()
+    {
+        return String.format(
+           FMT_STR,
+           this.getName(),
+           this.getDurability(),
+           this.getSpeed(),
+           this.getMaterial(),
+           this.getModifier(),
+           this.getModifierLevel()
+        );
+    }
+}
